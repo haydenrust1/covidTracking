2db351e9b47377440f12682e692c886f315696d0
Haydenrust1 <haydenrust1@gmail.com>
2020-08-19 16:31:44 -0700

    COVID 19 API Testing

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/***************************
+ * URL to COVID 19 API Documentation:
+ * https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest#071be6ab-ebcc-40dc-be8b-9209ab7caca5
+ * Recommend: 'By Country All Status'
+ ****************************/
+
+$(document).ready(function () {
+  //COVID 19 API
+
+  $.ajax({
+    url: 'https://api.covid19api.com/summary',
+    method: 'GET',
+  })
+    // After the data comes back from the API
+    .then(function (response) {
+      console.log(response);
+    });
+
+  $.ajax({
+    url: `https://api.covid19api.com/country/south-africa?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z`,
+    method: 'GET',
+  })
+    // After the data comes back from the API
+    .then(function (response) {
+      console.log(response);
+    });
+});
