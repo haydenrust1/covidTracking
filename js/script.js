8392f96785bb31c5bd3a58c04df00a7eba7f7046
Haydenrust1 <haydenrust1@gmail.com>
2020-08-27 16:04:55 -0700

    formatting

--- js/script.js
+++ js/script.js
@@ -33,35 +33,22 @@ $(document).ready(function () {
         countryCodeList.push(response.Countries[i].CountryCode);
       }
 
-      $('#country').autocomplete({
-        source: countriesList,
+      $("#country").autocomplete({
+        source: countriesList
       });
 
       // getGlobalPop(); // disabled to hardcode population
       GlobalNewConfirmed = response.Global.NewConfirmed;
       GlobalTotalConfirmed = response.Global.TotalConfirmed;
-      graphData(
-        'global-chart',
-        [
-          TotalConfirmed,
-          GlobalTotalConfirmed,
-          NewConfirmed,
-          GlobalNewConfirmed,
-        ],
-        [
-          'Country Total Confirmed',
-          'Global Total Confirmed',
-          'Country New Confirmed',
-          'Global New Confirmed',
-        ]
-      );
+      graphData("global-chart", [TotalConfirmed, GlobalTotalConfirmed, NewConfirmed, GlobalNewConfirmed], ["Country Total Confirmed", "Global Total Confirmed", "Country New Confirmed", "Global New Confirmed"]);
+
     });
 
   // On search button click
-  $('#searchBtn').on('click', function (event) {
+  $("#searchBtn").on("click", function (event) {
     event.preventDefault();
-    countryQuery = $(this).siblings('#country').val();
-    idx = covidQuery.Countries.findIndex((x) => x.Country === countryQuery);
+    countryQuery = $(this).siblings("#country").val();
+    idx = covidQuery.Countries.findIndex(x => x.Country === countryQuery);
     TotalConfirmed = covidQuery.Countries[idx].TotalConfirmed;
     countryCode = covidQuery.Countries[idx].CountryCode;
     NewConfirmed = covidQuery.Countries[idx].NewConfirmed;
@@ -70,19 +57,13 @@ $(document).ready(function () {
     // graphData("country-chart", [GlobalNewConfirmed, GlobalTotalConfirmed], ["Global New Confirmed", "Global Total Confirmed"]);
     // Alike data
 
-    graphData(
-      'global-chart',
-      [TotalConfirmed, GlobalTotalConfirmed, NewConfirmed, GlobalNewConfirmed],
-      [
-        'Country Total Confirmed',
-        'Global Total Confirmed',
-        'Country New Confirmed',
-        'Global New Confirmed',
-      ]
-    );
+    graphData("global-chart", [TotalConfirmed, GlobalTotalConfirmed, NewConfirmed, GlobalNewConfirmed], ["Country Total Confirmed", "Global Total Confirmed", "Country New Confirmed", "Global New Confirmed"]);
 
     getCountryPopulation(countryCode);
-  });
+
+  })
+
+
 
   // Call to get global population
   // function getGlobalPop() {
@@ -112,26 +93,30 @@ $(document).ready(function () {
   //         });
   // }
 
+
+
+
+
   // Call to get country population from query
   function getCountryPopulation(query) {
     var settings = {
-      async: true,
-      crossDomain: true,
-      url:
-        'https://world-geo-data.p.rapidapi.com/countries/' +
-        query +
-        '?language=en%252Cru%252Czh%252Ces%252Car%252Cfr%252Cfa%252Cja%252Cpl%252Cit%252Cpt%252Cde&format=json',
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'world-geo-data.p.rapidapi.com',
-        'x-rapidapi-key': '7890d205b1msh01e2a11ec28f854p1c50e9jsn12a3a11721e5',
-      },
-    };
+      "async": true,
+      "crossDomain": true,
+      "url": "https://world-geo-data.p.rapidapi.com/countries/" + query + "?language=en%252Cru%252Czh%252Ces%252Car%252Cfr%252Cfa%252Cja%252Cpl%252Cit%252Cpt%252Cde&format=json",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "world-geo-data.p.rapidapi.com",
+        "x-rapidapi-key": "7890d205b1msh01e2a11ec28f854p1c50e9jsn12a3a11721e5"
+      }
+    }
 
     $.ajax(settings).done(function (response) {
       countryPopulation = response.population;
+
     });
   }
+
+
 
   // graph display
   function graphData(id, data, names) {
@@ -140,41 +125,37 @@ $(document).ready(function () {
       type: 'bar',
       data: {
         labels: names,
-        datasets: [
-          {
-            label: '# of Votes',
-            data: data,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
+        datasets: [{
+          label: '# of Votes',
+          data: data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
       },
       options: {
         scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
     });
   }
 
@@ -182,37 +163,31 @@ $(document).ready(function () {
   getNews();
 
   function truncate(text, word_count) {
-    return text.split(' ').slice(0, word_count).join(' ');
+    return text.split(" ").slice(0, word_count).join(" ");
   }
 
   function getNews() {
     var settings = {
-      async: true,
-      crossDomain: true,
-      url:
-        'https://covid-19-news.p.rapidapi.com/v1/covid?lang=en&media=True&q=covid',
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'covid-19-news.p.rapidapi.com',
-        'x-rapidapi-key': '7890d205b1msh01e2a11ec28f854p1c50e9jsn12a3a11721e5',
-      },
-    };
+      "async": true,
+      "crossDomain": true,
+      "url": "https://covid-19-news.p.rapidapi.com/v1/covid?lang=en&media=True&q=covid",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "covid-19-news.p.rapidapi.com",
+        "x-rapidapi-key": "7890d205b1msh01e2a11ec28f854p1c50e9jsn12a3a11721e5"
+      }
+    }
     // After the data comes back from the API
     $.ajax(settings).done(function (response) {
       let randNum = Math.floor(Math.random() * 46);
 
       for (var i = 0; i < 4; i++) {
         // Populate articles and links
-        $('.card .card-body .card-title')
-          .eq(i)
-          .html(response.articles[randNum + i].title);
-        $('.card .card-body .card-text')
-          .eq(i)
-          .html(truncate(response.articles[randNum + i].summary, 50) + '...');
-        $('.card .card-body .btn-primary')
-          .eq(i)
-          .attr('href', response.articles[randNum + i].link);
+        $(".card .card-body .card-title").eq(i).html(response.articles[randNum + i].title);
+        $(".card .card-body .card-text").eq(i).html(truncate(response.articles[randNum + i].summary, 50) + "...");
+        $(".card .card-body .btn-primary").eq(i).attr("href", response.articles[randNum + i].link);
       }
+
     });
   }
 
